0956eb7aa6ec4ee22c4e470382c525bc372cae68
LucaArcudi <arcudilu@gmail.com>
2022-11-28 16:41:02 +0100

    ..

---
+++ snack-1/JS/script.js
@@ -0,0 +1,15 @@
+// Continuare a chiedere all'utente un numero tra 1 e 10 fino a quando l'utente non inserisce il valore corretto
+
+
+let userNumber = parseInt(prompt("Inserisci un numero"));
+
+let number = (0 < userNumber < 11);
+
+while (userNumber != number){
+    let userNumber = parseInt(prompt("Inserisci un numero"));
+
+    0 < userNumber < 11;
+}
+
+
+
