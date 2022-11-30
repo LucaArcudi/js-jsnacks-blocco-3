063aab35b97520115399befcc3efdb4443286fab
LucaArcudi <arcudilu@gmail.com>
2022-11-30 11:59:35 +0100

    snack-8 sol

---
+++ snack-8/JS/script.js
@@ -0,0 +1,43 @@
+/**
+ * Crea due tag div con due id diversi.
+ * Un div avrà il testo colorato di rosso mentre l'altro di verde.
+ * Partendo da un array di numeri,
+ * creiamo un nuovo elemento p e lo stampiamo nell'div rosso  se è un numero dispari
+ * e nel div verde se è un numero pari.
+ *
+ *
+ */
+
+ //  
+ // funzione che crei un nuovo elemento <p> </p>
+ // restituirlo a chi invoca la funzione
+ //
+ // ciclo per ogni elemento dell'array
+ // se l'elemento è un numero pari, crei nuovo elemento e lo aggiungo al div verde
+ // se l'elemento è un numero dispari, creo  nuovo elemento e lo aggiungo al div rosso
+
+
+
+const array = [1,33,55,64,33,22,10,44];
+
+function getNewElement (content, elementTag){
+    const newEl = document.createElement(elementTag);
+    newEl.innerText = content;
+    newEl.classList.add(classesToAdd);
+    return newEl;
+}
+ 
+let divRedOddElement = document.getElementById("text-red");
+let divGreenEvenElement = document.getElementById("text-green");
+
+for (let i = 0; i < array.length; i++){
+    if (array[i] % 2 === 0){
+        divRedOddElement.appendChild(getNewElement(array[i]));
+    } else {
+        divGreenEvenElement.appendChild(getNewElement(array[i]));
+    }
+}
+
+
+
+
