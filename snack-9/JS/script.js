4f54344d67884759c37553b3683c946a1fdc6830
LucaArcudi <arcudilu@gmail.com>
2022-12-01 12:13:16 +0100

    fix snack-9 sol

--- snack-9/JS/script.js
+++ snack-9/JS/script.js
@@ -9,14 +9,24 @@
 
 
 const startingList = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-let newList = [];
 
-for(let i = 0; i < startingList.length - 10; i++){
+const newList = [];
+
+for(let i = 0; i < 10; i++){
+
+    let randomNumber = getRandomNumber (0, startingList.length - 1);
+
+    newList [i] = startingList[Math.floor(Math.random() * startingList[randomNumber])];
 
 
-    newList [i] = startingList[Math.floor(Math.random() * i)];
+}
 
+function getRandomNumber (min, max){
+    if (min === max){
+        return max
+    }
 
+    return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 console.log(newList);
